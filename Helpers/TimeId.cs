cefdd1b6f04415c5fe8346afd7cd538aa09131c3
Brian <48544476+Brian3010@users.noreply.github.com>
2025-10-23 00:44:49 +1100

    replace chatId with timeid

---
+++ Helpers/TimeId.cs
@@ -0,0 +1,17 @@
+﻿namespace JenianAPI.Helpers
+{
+  public class TimeId
+  {
+    private static long _last;
+
+    public static long UniqueTicks() {
+      while (true) {
+        long now = DateTime.UtcNow.Ticks;
+        long last = Volatile.Read(ref _last);
+        long next = Math.Max(now, last + 1);
+        if (Interlocked.CompareExchange(ref _last, next, last) == last)
+          return next;
+      }
+    }
+  }
+}
