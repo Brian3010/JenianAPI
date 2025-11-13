c7c7097c159259fb616eb0625652721d1f8985ea
Brian <briannguyenwg@gmail.com>
2025-11-13 15:28:43 +1100

    implemeting making CWH report

---
+++ Controllers/CWHController.cs
@@ -0,0 +1,30 @@
+﻿using JenianAPI.Dtos.CwhDtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JenianAPI.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class CWHController : ControllerBase
+  {
+    private readonly ILogger<CWHController> _logger;
+
+    public CWHController(ILogger<CWHController> logger) {
+      _logger = logger;
+    }
+
+
+
+    [HttpPost("eod-report")]
+    public async Task<IActionResult> handleReport([FromBody] CWHReportRequestDTOs CWHReportRequest) {
+
+      // process the request and make a report
+      // process the deliveryScreenshot
+      // send the report to telegram
+
+
+      return Ok();
+    }
+
+  }
+}
