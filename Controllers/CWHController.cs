86888589e13072c009e6ff2530f0ec719460fca3
Brian <48544476+Brian3010@users.noreply.github.com>
2025-11-19 02:49:19 +1100

    reading stock photo

--- Controllers/CWHController.cs
+++ Controllers/CWHController.cs
@@ -1,5 +1,8 @@
 ﻿using JenianAPI.Dtos.CwhDtos;
+using JenianAPI.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Text;
 
 namespace JenianAPI.Controllers
 {
@@ -8,20 +11,28 @@ namespace JenianAPI.Controllers
   public class CWHController : ControllerBase
   {
     private readonly ILogger<CWHController> _logger;
+    private readonly IParserService _parserService;
 
-    public CWHController(ILogger<CWHController> logger) {
+    public CWHController(ILogger<CWHController> logger, IParserService parserService) {
       _logger = logger;
+      _parserService = parserService;
     }
 
 
-
+    //[Authorize]
     [HttpPost("eod-report")]
     public async Task<IActionResult> HandleReport([FromBody] CWHReportRequestDTOs CWHReportRequest) {
       if (CWHReportRequest == null) {
         return BadRequest("Invalid report data.");
       }
       // process the StockUpdate - process the DeliveryScreenShots
-
+      if (CWHReportRequest.DeliveryScreenShots == null || CWHReportRequest.DeliveryScreenShots.Count == 0) {
+        return BadRequest("No delivery screenshots provided.");
+      }
+      // parse the photos into OCR TEXT
+      var result = HandleStockUpdate(CWHReportRequest.DeliveryScreenShots);
+      _logger.LogInformation("OCR Result: {OcrResult}", result);
+      // testing with POSTMAN
 
       // process the NightTasks
       // process the AislesFacing
@@ -30,6 +41,21 @@ namespace JenianAPI.Controllers
 
       return Ok();
     }
+
+    private async Task<string> HandleStockUpdate(List<IFormFile> deliveryScreenShots) {
+      StringBuilder allOcrText = new StringBuilder();
+      foreach (var formFile in deliveryScreenShots) {
+        if (formFile.Length > 0) {
+          using var memoryStream = new MemoryStream();
+          await formFile.CopyToAsync(memoryStream);
+          var fileBytes = memoryStream.ToArray();
+          var ocrText = await _parserService.ExtractTextFromPhotoAsync(fileBytes, CancellationToken.None);
+          allOcrText.AppendLine(ocrText);
+        }
+      }
+      return allOcrText.ToString();
+    }
+
 
   }
 }
