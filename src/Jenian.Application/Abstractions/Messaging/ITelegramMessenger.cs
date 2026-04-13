8285a4dd420b020f619bcec14ca0868c621a2d82
Brian <48544476+Brian3010@users.noreply.github.com>
2026-04-13 10:55:48 +1000

    Clean up

--- src/Jenian.Application/Abstractions/Messaging/ITelegramMessenger.cs
+++ src/Jenian.Application/Abstractions/Messaging/ITelegramMessenger.cs
@@ -1,7 +1,10 @@
+using Jenian.Application.Features.Telegram.Dtos;
+
 namespace Jenian.Application.Abstractions.Messaging
 {
   public interface ITelegramMessenger
   {
     Task SendMessageAsync(long chatId, string text, CancellationToken ct = default);
+
   }
 }
