ca4acf04e44a2cf54ab2d477ff1f0c5256220767
Brian <48544476+Brian3010@users.noreply.github.com>
2026-04-15 23:19:44 +1000

    add error handler when failed to initialise blob container

--- src/Jenian.Infrastructure/storage/BlobContainerInitialiser.cs
+++ src/Jenian.Infrastructure/storage/BlobContainerInitialiser.cs
@@ -2,6 +2,7 @@
 using Azure.Storage.Blobs;
 using Azure.Storage.Blobs.Models;
 using Jenian.Infrastructure.Storage;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
 namespace Jenian.Infrastructure.storage
@@ -28,11 +29,18 @@ namespace Jenian.Infrastructure.storage
       if (string.IsNullOrWhiteSpace(_options.ContainerName))
         throw new InvalidOperationException("AzureBlobStorage:ContainerName is missing.");
 
+
       var containerClient = _blobServiceClient.GetBlobContainerClient(_options.ContainerName);
 
-      await containerClient.CreateIfNotExistsAsync(
-          PublicAccessType.None,
-          cancellationToken: cancellationToken);
+
+      try {
+        await containerClient.CreateIfNotExistsAsync(
+         PublicAccessType.None,
+         cancellationToken: cancellationToken);
+      } catch (Exception ex) {
+        _logger.LogError(ex, "Failed to initialise blob container");
+      }
+
 
       _logger.LogInformation(
           "Verified blob container {ContainerName} exists.",
