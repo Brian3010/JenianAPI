4bc83f6a79d34cfbacbea4a8d68623de6825ba51
Brian <48544476+Brian3010@users.noreply.github.com>
2026-05-06 17:48:15 +1000

    Add UserShift and UserDailyPaySummary entities

---
+++ src/Jenian.Domain/Entities/UserDailyPaySummary.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jenian.Domain.Entities
+{
+  public class UserDailyPaySummary
+  {
+    public Guid Id { get; private set; } = Guid.NewGuid();
+
+
+    // one user + one local day
+    public DateOnly WorkDate { get; set; }
+
+    // total minutes contributing to pay
+    public int TotalPayableMinutes { get; set; }
+
+    // aggregated from UserShift
+    public int TotalPaidBreakMinutes { get; set; }
+    public int TotalUnpaidBreakMinutes { get; set; }
+
+    // calculation buckets
+    public int TotalEveningPenaltyMinutes { get; set; }
+    public int TotalOvertimeMinutes { get; set; }
+
+    public decimal BaseRateUsed { get; set; }
+    public decimal GrossPay { get; set; }
+
+    public DateTimeOffset CalculatedAtUtc { get; set; } = DateTimeOffset.UtcNow;
+
+    public required string UserId { get; set; }
+    public ICollection<UserShift> Shifts { get; set; } = new List<UserShift>();
+  }
+}
---
+++ src/Jenian.Domain/Entities/UserShift.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jenian.Domain.Entities
+{
+
+  public enum ShiftEntryType
+  {
+    Worked = 1,
+    PaidNonWorked = 2,
+    Leave = 3,
+  }
+
+  public enum EmploymentType
+  {
+    FullTime = 1,
+    PartTime = 2,
+    Casual = 3
+  }
+
+  public enum ShiftSource
+  {
+    Manual = 1,
+    OCR = 2,
+    Telegram = 3,
+    CsvImport = 4,
+    ApiImport = 5
+  }
+  public class UserShift
+  {
+    public Guid Id { get; private set; } = Guid.NewGuid();
+
+    public DateTimeOffset StartAt { get; set; }
+    public DateTimeOffset EndAt { get; set; }
+    public string TimeZoneId { get; set; } = "Australia/Melbourne";
+
+    public int UnpaidBreakMinutes { get; set; }
+    public int PaidBreakMinutes { get; set; }
+
+    public ShiftEntryType EntryType { get; set; } = ShiftEntryType.Worked;
+    public EmploymentType EmploymentType { get; set; }
+
+    public ShiftSource Source { get; set; } = ShiftSource.Manual;
+    public DateTimeOffset CreatedAtUtc { get; set; } = DateTimeOffset.UtcNow;
+    public DateTimeOffset UpdatedAtUtc { get; set; } = DateTimeOffset.UtcNow;
+
+    public required string UserId { get; set; }
+    public Guid? UserDailyPaySummaryId { get; set; }
+    public UserDailyPaySummary? UserDailyPaySummary { get; set; }
+
+  }
+}
