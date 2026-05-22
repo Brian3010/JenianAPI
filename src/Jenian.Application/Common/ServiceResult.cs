7aaeaf640cd3e962cec64136dff472815f8c968e
Brian <48544476+Brian3010@users.noreply.github.com>
2026-05-23 00:30:17 +1000

    Refactor namespaces and clean up unused usings

--- src/Jenian.Application/Common/ServiceResult.cs
+++ src/Jenian.Application/Common/ServiceResult.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Jenian.Application.Common
+﻿namespace Jenian.Application.Common
 {
   public class ServiceResult<T>
   {
