259fcef4279a2817ba7000139e09d0e423b712c1
Brian <48544476+Brian3010@users.noreply.github.com>
2026-05-23 02:47:05 +1000

    Add `IAwardRateService` interface with methods for pay calculation, time segmentation, and multiplier determination. Implemented `PharmacyAwardRateService` with detailed pay

---
+++ src/Jenian.Application/Features/Shifts/Services/IAwardRateService.cs
@@ -0,0 +1,38 @@
+﻿using Jenian.Application.Features.Shifts.Dtos;
+using Jenian.Domain.Entities;
+
+namespace Jenian.Application.Features.Shifts.Services
+{
+
+  public enum DayType
+  {
+    WeekDay,
+    Saturday,
+    Sunday
+  }
+  public record TimeSegment(
+  DateTimeOffset Start,
+  DateTimeOffset End,
+  int Hours
+);
+  public interface IAwardRateService
+  {
+    decimal GetMultiplier(
+      DateTimeOffset startTime,
+      EmploymentType employmentType,
+      bool isPublicHoliday,
+      ShiftEntryType shiftEntryType
+      );
+
+
+
+    List<TimeSegment> GetTimeSegmentsForShift(DateTimeOffset startTime, DateTimeOffset endTime);
+
+    decimal CalculateGrossPayForShift(
+      ShiftDto shiftDto,
+      bool isPublicHoliday,
+      decimal baseHourlyRate
+      );
+
+  }
+}
