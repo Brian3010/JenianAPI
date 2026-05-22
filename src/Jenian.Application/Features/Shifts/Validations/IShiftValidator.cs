8fbd5f9f8de6d6e5f35e186fa2e298d7faf322e9
Brian <48544476+Brian3010@users.noreply.github.com>
2026-05-23 00:33:40 +1000

    Add validation for shifts in ShiftService

---
+++ src/Jenian.Application/Features/Shifts/Validations/IShiftValidator.cs
@@ -0,0 +1,10 @@
+﻿using Jenian.Application.Common;
+using Jenian.Application.Features.Shifts.Dtos;
+
+namespace Jenian.Application.Features.Shifts.Validations
+{
+  public interface IShiftValidator
+  {
+    ValidationResult ValidateSaveShifts(List<ShiftDto> shitfs, DateOnly cycleStartDate, DateOnly cycleEndDate);
+  }
+}
---
+++ src/Jenian.Application/Features/Shifts/Validations/ShiftValidator.cs
@@ -0,0 +1,37 @@
+﻿using Jenian.Application.Common;
+using Jenian.Application.Features.Shifts.Dtos;
+
+namespace Jenian.Application.Features.Shifts.Validations
+{
+  public class ShiftValidator : IShiftValidator
+  {
+    public ValidationResult ValidateSaveShifts(List<ShiftDto> shitfs, DateOnly cycleStartDate, DateOnly cycleEndDate) {
+      List<string> errors = [];
+
+      foreach (var shift in shitfs) {
+        if (shift.StartAt >= shift.EndAt) {
+          errors.Add($"Shift with Id {shift.Id} has StartAt greater than or equal to EndAt.");
+        }
+        if (string.IsNullOrEmpty(shift.TimeZoneId)) {
+          errors.Add($"Shift with Id {shift.Id} has an empty TimeZoneId.");
+        }
+        var durationMinutes = (shift.EndAt - shift.StartAt).TotalMinutes;
+        var totalBreakMinutes = shift.UnpaidBreakMinutes + shift.PaidBreakMinutes;
+
+        if (totalBreakMinutes > durationMinutes) {
+          errors.Add($"Shift with Id {shift.Id} has break minutes exceeding shift duration.");
+        }
+
+        var startDate = DateOnly.FromDateTime(shift.StartAt.DateTime);
+        var endDate = DateOnly.FromDateTime(shift.EndAt.DateTime);
+
+        if (startDate < cycleStartDate || endDate > cycleEndDate) {
+          errors.Add($"Shift with Id {shift.Id} has StartAt or EndAt outside of the cycle date range.");
+        }
+
+      }
+
+      return errors.Count == 0 ? ValidationResult.Success() : ValidationResult.Failure(errors);
+    }
+  }
+}
