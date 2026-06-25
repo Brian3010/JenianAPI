af741e528fe44fcdefab2dadc7529ec138a5eb15
Brian <48544476+Brian3010@users.noreply.github.com>
2026-06-25 11:28:47 +1000

    add cookie names types

---
+++ src/Jenian.API/Auth/AuthCookieNames.cs
@@ -0,0 +1,9 @@
+﻿namespace Jenian.API.Auth
+{
+  public static class AuthCookieNames
+  {
+    public const string RefreshToken = "refreshToken";
+    public const string AccessToken = "accessToken";
+    public const string DeviceId = "deviceId";
+  }
+}
---
+++ src/Jenian.API/Auth/AuthCookieSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace Jenian.API.Auth
+{
+  public sealed class AuthCookieSettings
+  {
+    public int AccessTokenMinutes { get; init; }
+    public int RefreshTokenDays { get; init; }
+    public int DeviceIdDays { get; init; }
+  }
+}
