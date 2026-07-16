ce92b5414553b9124e2951475f0c10e75dc5f090
Brian <48544476+Brian3010@users.noreply.github.com>
2026-07-16 11:13:11 +1000

    Update base hourly rate in CalculateDailyPay to 27.81
    The base hourly rate in the CalculateDailyPay method was changed from 26.55 to 27.81. The rate remains hardcoded, with a note to retrieve it from the user profile or JWT claims in the future.

--- src/Jenian.Application/Features/Payroll/PayCalculator.cs
+++ src/Jenian.Application/Features/Payroll/PayCalculator.cs
@@ -24,7 +24,7 @@ namespace Jenian.Application.Features.Payroll
 
       // Note: 'VIC' later will be determined by user's location or shift location
       var isPublicHoliday = _publicHolidayService.IsPublicHoliday(DateOnly.FromDateTime(shifts[0].StartAt.DateTime), "VIC");
-      const decimal baseHourlyRate = 26.55m; //Note: latter need to get from user profile from database or jwt token claims
+      const decimal baseHourlyRate = 27.81m; //Note: latter need to get from user profile from database or jwt token claims
 
       var results = new List<TotalPaySummary>();
       foreach (var shift in shifts) {
